2215fa3be89e7a0842c7c2f25ca376b054dfd268
Mouktik Ghosh <moughosh2@publicisgroupe.net>
2022-09-19 12:42:34 +0530

    First commit

---
+++ Coding/src/test/java/com/example/Coding/CodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
